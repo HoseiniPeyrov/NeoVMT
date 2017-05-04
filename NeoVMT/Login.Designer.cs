b239e30bbc40cbfd63f8b7a150546f5b42ad7a38
hoseyni <hoseyni@HOSEYNI-PC>
2017-05-04 07:25:25 +0430

    Add project files.

---
+++ NeoVMT/Login.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace NeoVMT
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevComponents.DotNetBar.Keyboard.VirtualKeyboardColorTable virtualKeyboardColorTable10 = new DevComponents.DotNetBar.Keyboard.VirtualKeyboardColorTable();
+            DevComponents.DotNetBar.Keyboard.FlatStyleRenderer flatStyleRenderer10 = new DevComponents.DotNetBar.Keyboard.FlatStyleRenderer();
+            this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.keyboardControl1 = new DevComponents.DotNetBar.Keyboard.KeyboardControl();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialContextMenuStrip1 = new MaterialSkin.Controls.MaterialContextMenuStrip();
+            this.vnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialContextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialSingleLineTextField2
+            // 
+            this.materialSingleLineTextField2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialSingleLineTextField2.Depth = 0;
+            this.materialSingleLineTextField2.Hint = "";
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(454, 109);
+            this.materialSingleLineTextField2.MaxLength = 32767;
+            this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
+            this.materialSingleLineTextField2.PasswordChar = '\0';
+            this.materialSingleLineTextField2.SelectedText = "";
+            this.materialSingleLineTextField2.SelectionLength = 0;
+            this.materialSingleLineTextField2.SelectionStart = 0;
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(84, 23);
+            this.materialSingleLineTextField2.TabIndex = 5;
+            this.materialSingleLineTextField2.TabStop = false;
+            this.materialSingleLineTextField2.UseSystemPasswordChar = false;
+            this.materialSingleLineTextField2.Click += new System.EventHandler(this.materialSingleLineTextField2_Click);
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(454, 165);
+            this.materialSingleLineTextField1.MaxLength = 32767;
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(84, 23);
+            this.materialSingleLineTextField1.TabIndex = 6;
+            this.materialSingleLineTextField1.TabStop = false;
+            this.materialSingleLineTextField1.UseSystemPasswordChar = true;
+            this.materialSingleLineTextField1.Click += new System.EventHandler(this.materialSingleLineTextField1_Click);
+            // 
+            // keyboardControl1
+            // 
+            this.keyboardControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            virtualKeyboardColorTable10.BackgroundColor = System.Drawing.Color.Black;
+            virtualKeyboardColorTable10.DarkKeysColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(28)))), ((int)(((byte)(33)))));
+            virtualKeyboardColorTable10.DownKeysColor = System.Drawing.Color.White;
+            virtualKeyboardColorTable10.DownTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            virtualKeyboardColorTable10.KeysColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(47)))), ((int)(((byte)(55)))));
+            virtualKeyboardColorTable10.LightKeysColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(68)))), ((int)(((byte)(76)))));
+            virtualKeyboardColorTable10.PressedKeysColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(161)))), ((int)(((byte)(81)))));
+            virtualKeyboardColorTable10.TextColor = System.Drawing.Color.White;
+            virtualKeyboardColorTable10.ToggleTextColor = System.Drawing.Color.Green;
+            virtualKeyboardColorTable10.TopBarTextColor = System.Drawing.Color.White;
+            this.keyboardControl1.ColorTable = virtualKeyboardColorTable10;
+            this.keyboardControl1.Location = new System.Drawing.Point(454, 277);
+            this.keyboardControl1.Name = "keyboardControl1";
+            flatStyleRenderer10.ColorTable = virtualKeyboardColorTable10;
+            flatStyleRenderer10.ForceAntiAlias = false;
+            this.keyboardControl1.Renderer = flatStyleRenderer10;
+            this.keyboardControl1.Size = new System.Drawing.Size(84, 250);
+            this.keyboardControl1.TabIndex = 7;
+            this.keyboardControl1.Text = "keyboardControl1";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(367, 113);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(81, 18);
+            this.materialLabel1.TabIndex = 8;
+            this.materialLabel1.Text = "Username:";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(367, 165);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(79, 18);
+            this.materialLabel2.TabIndex = 9;
+            this.materialLabel2.Text = "Password:";
+            // 
+            // materialContextMenuStrip1
+            // 
+            this.materialContextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialContextMenuStrip1.Depth = 0;
+            this.materialContextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vnToolStripMenuItem});
+            this.materialContextMenuStrip1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialContextMenuStrip1.Name = "materialContextMenuStrip1";
+            this.materialContextMenuStrip1.Size = new System.Drawing.Size(88, 26);
+            // 
+            // vnToolStripMenuItem
+            // 
+            this.vnToolStripMenuItem.Name = "vnToolStripMenuItem";
+            this.vnToolStripMenuItem.Size = new System.Drawing.Size(87, 22);
+            this.vnToolStripMenuItem.Text = "vn";
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Icon = global::NeoVMT.Properties.Resources.key;
+            this.materialFlatButton1.Location = new System.Drawing.Point(626, 218);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = true;
+            this.materialFlatButton1.Size = new System.Drawing.Size(149, 36);
+            this.materialFlatButton1.TabIndex = 11;
+            this.materialFlatButton1.Text = "Login";
+            this.materialFlatButton1.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 519);
+            this.ContextMenuStrip = this.materialContextMenuStrip1;
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.keyboardControl1);
+            this.Controls.Add(this.materialSingleLineTextField1);
+            this.Controls.Add(this.materialSingleLineTextField2);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login To NeoVMT";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.materialContextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private DevComponents.DotNetBar.Keyboard.KeyboardControl keyboardControl1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialContextMenuStrip materialContextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem vnToolStripMenuItem;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+    }
+}
---
+++ NeoVMT/Login.cs
@@ -0,0 +1,90 @@
+﻿using DevComponents.DotNetBar.Keyboard;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeoVMT
+{
+    public partial class Login : MaterialForm
+    {
+        private readonly MaterialSkinManager materialSkinManager;
+        public Login()
+        {
+            InitializeComponent();
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Amber800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            keyboardControl1.Keyboard = CreateNumericKeyboard();
+
+
+        }
+        private Keyboard CreateNumericKeyboard()
+        {
+            Keyboard keyboard = new Keyboard();
+
+            LinearKeyboardLayout klNumLockOn = new LinearKeyboardLayout();
+
+            
+
+            klNumLockOn.AddKey("7");
+            klNumLockOn.AddKey("8");
+            klNumLockOn.AddKey("9");
+          
+            klNumLockOn.AddLine();
+
+            klNumLockOn.AddKey("4");
+            klNumLockOn.AddKey("5");
+            klNumLockOn.AddKey("6");
+            klNumLockOn.AddLine();
+
+            klNumLockOn.AddKey("1");
+            klNumLockOn.AddKey("2");
+            klNumLockOn.AddKey("3");
+            klNumLockOn.AddLine();
+
+            klNumLockOn.AddKey("0", width: 21);
+  
+            klNumLockOn.AddKey("Enter", "{ENTER}", height: 21);
+
+
+
+
+
+            keyboard.Layouts.Add(klNumLockOn);
+
+
+            return keyboard;
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            keyboardControl1.Show();
+
+        }
+
+        private void materialSingleLineTextField2_Click(object sender, EventArgs e)
+        {
+            keyboardControl1.Show();
+
+        }
+
+        private void materialSingleLineTextField1_Click(object sender, EventArgs e)
+        {
+            keyboardControl1.Show();
+
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            keyboardControl1.Show();
+        }
+    }
+}
